25e77ce915e3d85446b298e223e957b8f4f026a8
waka3 <a420926205@163.com>
2020-10-28 11:25:58 +0800

    feat: createElement 解析并显示自定义标签+ HTML标签

--- week13/JSX/NOTE.md
+++ week13/JSX/NOTE.md
@@ -162,3 +162,56 @@ React.createElement(\"a\", {\n  href: \"//m.taobao.com\"\n}
 
   dom.mountTo(document.body);
   ```
+
+4. 普通标签 和 自定义标签均可以用 mountTo 添加到并展示
+  ```js
+  function createElement(tagName, attributes, ...children) {
+    let element;
+    if (typeof tagName === 'string') {
+      element = new ElementWrapper(tagName);
+    } else {
+      element = new tagName; // 非 HTML 标签的返回的的tagName 为 function
+    }
+    for (let name in attributes) {
+      element.setAttribute(name, attributes[name]);
+    }
+    for (let child of children) {
+      if (typeof child === 'string') { // 文本节点解析
+        child = new TextWrapper(child);
+      }
+      element.appendChild(child);
+    }
+    return element;
+  }
+  // 创建 HTML 元素
+  class ElementWrapper{
+    constructor(tag) {
+      this.root = document.createElement(tag);
+    }
+    setAttribute(name, value) {
+      this.root.setAttribute(name, value);
+    }
+    appendChild(child) {
+      child.mountTo(this.root);
+    }
+    mountTo(parent) {
+      parent.appendChild(this.root);
+    }
+  }
+
+  // 创建 Content 元素
+  class TextWrapper{
+    constructor(tag) {
+      this.root = document.createTextNode(tag);
+    }
+    setAttribute(name, value) {
+      this.root.setAttribute(name, value);
+    }
+    appendChild(child) {
+      child.mountTo(this.root);
+    }
+    mountTo(parent) {
+      parent.appendChild(this.root);
+    }
+  }
+  ```
--- week13/JSX/main.js
+++ week13/JSX/main.js
@@ -1,7 +1,7 @@
 function createElement(tagName, attributes, ...children) {
   let element;
   if (typeof tagName === 'string') {
-    element = document.createElement(tagName);
+    element = new ElementWrapper(tagName);
   } else {
     element = new tagName; // 非 HTML 标签的返回的的tagName 为 function
   }
@@ -10,11 +10,41 @@ function createElement(tagName, attributes, ...children) {
   }
   for (let child of children) {
     if (typeof child === 'string') { // 文本节点解析
-      child = document.createTextNode(child);
+      child = new TextWrapper(child);
     }
     element.appendChild(child);
   }
   return element;
+}
+
+class ElementWrapper{
+  constructor(tag) {
+    this.root = document.createElement(tag);
+  }
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value);
+  }
+  appendChild(child) {
+    child.mountTo(this.root);
+  }
+  mountTo(parent) {
+    parent.appendChild(this.root);
+  }
+}
+
+class TextWrapper{
+  constructor(tag) {
+    this.root = document.createTextNode(tag);
+  }
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value);
+  }
+  appendChild(child) {
+    child.mountTo(this.root);
+  }
+  mountTo(parent) {
+    parent.appendChild(this.root);
+  }
 }
 
 class DIV{
@@ -25,7 +55,7 @@ class DIV{
     this.root.setAttribute(name, value);
   }
   appendChild(child) {
-    this.root.appendChild(child);
+    child.mountTo(this.root);
   }
   mountTo(parent) {
     parent.appendChild(this.root);
@@ -39,6 +69,4 @@ const dom = <DIV id="a">
   <span>d</span>
 </DIV>
 
-// document.body.appendChild(dom);
-// 反向思路：把节点塞到某个节点下
 dom.mountTo(document.body);
