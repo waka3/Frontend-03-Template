31ef28742a1001c547a9acf33c150e8281973759
waka3 <a420926205@163.com>
2020-11-22 23:49:12 +0800

    feat: yeoman 输入输出

--- week17/generators/app/index.js
+++ week17/generators/app/index.js
@@ -6,11 +6,22 @@ module.exports = class extends Generator {
     // Calling the super constructor is important so our generator is correctly set up
     super(args, opts);
   }
-  method1() {
-    this.log('method 1 just ran');
-  }
+  async prompting() {
+    const answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname // Default to current folder name
+      },
+      {
+        type: "confirm",
+        name: "cool",
+        message: "Would you like to enable the Cool feature?"
+      }
+    ]);
 
-  method2() {
-    this.log('method 2 just ran');
+    this.log("app name", answers.name);
+    this.log("cool feature", answers.cool);
   }
 };
